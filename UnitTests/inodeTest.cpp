ba36e60827d8c75d6638a55f058fa927c98da522
Jonathan Ström <jonathan.c.strom@gmail.com>
2017-10-10 15:01:25 +0200

    Added UnitTests project.

---
+++ UnitTests/inodeTest.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{		
+	TEST_CLASS(inodeTest)
+	{
+	public:
+	};
+}
